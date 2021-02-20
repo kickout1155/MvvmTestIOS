913ed34c24364687675796a873b63f5676fa6c85
alexey <kickout1155@gmail.com>
2021-02-20 18:06:02 +0300

    init

---
+++ Shared/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Alex on 20.02.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/MvvmTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  MvvmTestApp.swift
+//  Shared
+//
+//  Created by Alex on 20.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct MvvmTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
